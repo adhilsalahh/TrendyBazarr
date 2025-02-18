17e76c2da58cb9caf7bf092bf0255eecdea189af
adhilsalahh <adhilsalahhk@gmail.com>
2025-02-18 23:27:37 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/",
+  base: '/TrendyBazarr/',
   optimizeDeps: {
     exclude: ['lucide-react']
   },
