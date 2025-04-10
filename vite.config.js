645f6aa79163d8d5024962d6752eb6d2fcd4cf27
adhilsalahh <adhilsalahhk@gmail.com>
2025-04-10 16:44:09 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   base: '/TrendyBazarr/',
   optimizeDeps: {
-    exclude: ['lucide-react']
+    exclude: ['lucide-react'],
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': `${__dirname}/src`,
     },
-    extensions: ['.js', '.jsx']
-  }
+    extensions: ['.js', '.jsx'],
+  },
 });
